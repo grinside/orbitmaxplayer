3cbe327d7241c7d88f4befe25c8e0f3d85896ebb
grinside <105011825+grinside@users.noreply.github.com>
2025-03-20 13:38:04 +0100

    a

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -3,7 +3,7 @@ export function register() {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/service-worker.js')
         .then(reg => console.log('✅ Service Worker enregistré avec succès !', reg))
-        .catch(err => console.error('❌ Erreur Service Worker :', err));
+        .catch(err => console.error('❌ Erreur Service Workers :', err));
     });
   }
 }
