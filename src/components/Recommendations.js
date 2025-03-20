0661a6f52ae3afcad12631ede5a918691ff421c1
grinside <105011825+grinside@users.noreply.github.com>
2025-03-20 14:57:54 +0100

    r

---
+++ src/components/Recommendations.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Recommendations = () => {
+  return (
+    <div className="recommendations">
+      <h2>📌 Recommandations pour vous</h2>
+      <p>Suggestions basées sur votre activité</p>
+    </div>
+  );
+};
+
+export default Recommendations;
